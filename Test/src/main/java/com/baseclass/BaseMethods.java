9c499dc81c911bc57e1740ec14bf53a71075fd2d
DELL <DELL@Murali>
2024-06-22 03:34:15 +0530

    timestamp added in extent report

--- Test/src/main/java/com/baseclass/BaseMethods.java
+++ Test/src/main/java/com/baseclass/BaseMethods.java
@@ -1,6 +1,15 @@
 package com.baseclass;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 import com.relevantcodes.extentreports.ExtentReports;
 import com.relevantcodes.extentreports.ExtentTest;
 import com.relevantcodes.extentreports.LogStatus;
@@ -10,7 +19,9 @@ public class BaseMethods {
 	public WebDriver driver;
 	public ExtentReports reports = null;
 	public ExtentTest test = null;
+	public WebDriverWait wait;
 
+	// driver info
 	public void driverinfo(WebDriver driver) {
 
 		String URL = "https://www.alfadock-pack.com/";
@@ -18,52 +29,70 @@ public class BaseMethods {
 
 	}
 
-//	public int webdriverwait(int sec, String name) {
-//		
-//		// Create WebDriverWait instance with a timeout of 10 seconds
-//		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(sec));
-//
-//		// Wait until the element with id "myButton" is clickable
-//		wait.until(ExpectedConditions.elementToBeClickable(name)));
-//		return sec;
-//	}
-
+	// close the browser
 	public void browserclose(WebDriver driver) throws InterruptedException {
 		Thread.sleep(2000);
 		driver.quit();
 	}
 
+	// extent report path
 	public void setupExtentReport(WebDriver driver) {
-		reports = new ExtentReports("F:\\ScreenShotsAndExtentReport\\ExtentReport\\Report.html", true);
+		// Get current timestamp
+        LocalDateTime currentTime = LocalDateTime.now();
+        
+        // Format timestamp (optional)
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd_HH-mm-ss");
+        String timestamp = currentTime.format(formatter);
+        
+		reports = new ExtentReports("F:\\ScreenShotsAndExtentReport\\ExtentReport\\Report_"+timestamp+".html", true);
 
 	}
 
+	// extent report start test
 	public void startTest(WebDriver driver, String testName) {
 		test = reports.startTest(testName);
 
 	}
 
+	// extent report logStatus.INFO
 	public void logInfo(WebDriver driver, String message) {
 		test.log(LogStatus.INFO, message);
 
 	}
 
+	// extent report logStatus.PASS
 	public void logPASS(WebDriver driver, String message) {
 		test.log(LogStatus.PASS, message);
 
 	}
 
+	// extent report logStatus.FAIL
 	public void logFAIL(WebDriver driver, String message) {
 		test.log(LogStatus.FAIL, message);
 
 	}
 
+	// extent report end test
 	public void endTest(WebDriver driver) {
 		reports.endTest(test);
 	}
 
+	// extent report end and generate the report.
 	public void endreport(WebDriver driver) {
 		reports.flush();
 	}
+	
+	public Duration wait(WebDriver driver,Duration sec) {
+        wait = new WebDriverWait(driver, sec); // Initialize WebDriverWait with a timeout of 10 seconds
+		return sec;
+    }
+	
+	public WebElement waitForElementToBeClickable(By locator) {
+        return wait.until(ExpectedConditions.elementToBeClickable(locator));
+    }
+
+    public WebElement waitForElementToBeVisible(By locator) {
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
 
 }
--- Test/src/test/java/com/testScenarios/LoginTestCases.java
+++ Test/src/test/java/com/testScenarios/LoginTestCases.java
@@ -2,13 +2,12 @@ package com.testScenarios;
 
 import java.time.Duration;
 
-import org.openqa.selenium.Alert;
+import org.apache.commons.mail.EmailException;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
@@ -16,6 +15,7 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import com.baseclass.BaseMethods;
+import com.baseclass.MailWithAttachment;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -23,6 +23,7 @@ public class LoginTestCases extends BaseMethods {
 
 	public WebDriver driver;
 	BaseMethods bm = new BaseMethods();
+	WebDriverWait wait;
 
 	@BeforeClass
 	public void setUp() {
@@ -45,10 +46,11 @@ public class LoginTestCases extends BaseMethods {
 		bm.startTest(driver, "comp_login_With_Valid_Credentials");
 
 		// Create WebDriverWait instance with a timeout of 10 seconds
-		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+		bm.wait(driver, Duration.ofSeconds(20));
 
 		// Wait until the element with id "myButton" is clickable
-		wait.until(ExpectedConditions.elementToBeClickable(By.id("username")));
+		bm.waitForElementToBeClickable(By.id("username"));
+
 		// Enter username
 		WebElement usernameField = driver.findElement(By.id("username"));
 		usernameField.sendKeys("Atkgi");
@@ -63,17 +65,18 @@ public class LoginTestCases extends BaseMethods {
 		WebElement loginButton = driver.findElement(By.id("logmein"));
 		loginButton.click();
 		bm.logInfo(driver, "login button clicked");
+		bm.wait(driver, Duration.ofSeconds(20));
+		bm.waitForElementToBeVisible(By.id("username"));
 		Thread.sleep(3000);
-
 		WebElement logoutbutton = driver.findElement(By.id("logout"));
 		if (logoutbutton.isDisplayed() == true) {
 			bm.logPASS(driver, "login sucessfull");
-			Thread.sleep(3000);
 			logoutbutton.click();
+			Thread.sleep(3000);
 		} else {
 			bm.logFAIL(driver, "login failed");
 		}
-		
+
 	}
 
 	@Test(enabled = true, priority = 2, testName = "comp_login_With_Invalid_Credentials")
@@ -82,38 +85,161 @@ public class LoginTestCases extends BaseMethods {
 		bm.startTest(driver, "comp_login_With_Invalid_Credentials");
 
 		// Create WebDriverWait instance with a timeout of 10 seconds
-		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+		bm.wait(driver, Duration.ofSeconds(20));
 
 		// Wait until the element with id "myButton" is clickable
-		wait.until(ExpectedConditions.elementToBeClickable(By.id("username")));
+		bm.waitForElementToBeClickable(By.id("username"));
 
 		// Enter username
 		WebElement usernameField = driver.findElement(By.id("username"));
 		usernameField.sendKeys("Atkgi");
 		bm.logInfo(driver, "username entered");
+		bm.wait(driver, Duration.ofSeconds(5));
 
 		// Enter password
 		WebElement passwordField = driver.findElement(By.id("password"));
 		passwordField.sendKeys("123");
 		bm.logInfo(driver, "password entered");
+		bm.wait(driver, Duration.ofSeconds(5));
 
 		// Click login button
 		WebElement loginButton = driver.findElement(By.id("logmein"));
 		loginButton.click();
+		bm.wait(driver, Duration.ofSeconds(20));
 		bm.logInfo(driver, "login button clicked");
 		bm.logInfo(driver, "popup displayed");
 		Thread.sleep(3000);
 		By dialogbox = By.xpath("//h4[text()='ログインエラー']/following::button[text()='OK' or @class='btn btn-default']");
 		driver.findElement(dialogbox).click();
 		bm.logInfo(driver, "popup closed");
-		
+		Thread.sleep(2000);
+
+	}
+
+	@Test(enabled = true, priority = 3, testName = "user_login_With_Invalid_Credentials")
+	public void userLoginWithInvalidCredentials() throws InterruptedException {
+
+		bm.startTest(driver, "user_login_With_Invalid_Credentials");
+
+		Thread.sleep(2000);
+		driver.navigate().refresh();
+
+		// Create WebDriverWait instance with a timeout of 10 seconds
+		bm.wait(driver, Duration.ofSeconds(20));
+
+		// Wait until the element with id "myButton" is clickable
+		bm.waitForElementToBeClickable(By.id("username"));
+
+		// Enter username
+		WebElement usernameField = driver.findElement(By.id("username"));
+		usernameField.sendKeys("Atkgi");
+		bm.logInfo(driver, "comp login username entered");
+
+		// Enter password
+		WebElement passwordField = driver.findElement(By.id("password"));
+		passwordField.sendKeys("1234");
+		bm.logInfo(driver, "comp login password entered");
+
+		// Click login button
+		WebElement loginButton = driver.findElement(By.id("logmein"));
+		loginButton.click();
+		bm.logInfo(driver, "comp login login button clicked");
+		bm.wait(driver, Duration.ofSeconds(20));
+		bm.waitForElementToBeVisible(By.id("username"));
+		Thread.sleep(3000);
+
+		// Create WebDriverWait instance with a timeout of 10 seconds
+		bm.wait(driver, Duration.ofSeconds(20));
+
+		// Wait until the element with id "myButton" is clickable
+		bm.waitForElementToBeClickable(By.id("username"));
+
+		// Enter username
+		usernameField = driver.findElement(By.id("username"));
+		usernameField.sendKeys("admi");
+		bm.logInfo(driver, "user page username entered");
+		bm.wait(driver, Duration.ofSeconds(5));
+
+		// Enter password
+		passwordField = driver.findElement(By.id("password"));
+		passwordField.sendKeys("admin");
+		bm.logInfo(driver, "user page password entered");
+		bm.wait(driver, Duration.ofSeconds(5));
+
+		// Click login button
+		loginButton = driver.findElement(By.id("login"));
+		loginButton.click();
+		bm.wait(driver, Duration.ofSeconds(20));
+		bm.logInfo(driver, "user page login button clicked");
+		bm.logInfo(driver, "popup displayed");
+		Thread.sleep(3000);
+		By dialogbox = By.xpath("//h4[text()='ログインエラー']/following::button[text()='OK' or @class='btn btn-default']");
+		driver.findElement(dialogbox).click();
+		bm.logInfo(driver, "popup closed");
+		Thread.sleep(2000);
+
+	}
+
+	@Test(enabled = true, priority = 4, testName = "user_login_With_valid_Credentials")
+	public void userLoginWithValidCredentials() throws InterruptedException {
+
+		driver.navigate().refresh();
+		Thread.sleep(2000);
+		bm.startTest(driver, "user_login_With_valid_Credentials");
+
+		// Create WebDriverWait instance with a timeout of 10 seconds
+		bm.wait(driver, Duration.ofSeconds(20));
+
+		// Wait until the element with id "myButton" is clickable
+		bm.waitForElementToBeClickable(By.id("username"));
+
+		// Enter username
+		WebElement usernameField = driver.findElement(By.id("username"));
+		usernameField.sendKeys("admin");
+		bm.logInfo(driver, "username entered");
+		bm.wait(driver, Duration.ofSeconds(5));
+
+		// Enter password
+		WebElement passwordField = driver.findElement(By.id("password"));
+		passwordField.sendKeys("admin");
+		bm.logInfo(driver, "password entered");
+		bm.wait(driver, Duration.ofSeconds(5));
+
+		// Click login button
+		WebElement loginButton = driver.findElement(By.id("login"));
+		loginButton.click();
+		bm.wait(driver, Duration.ofSeconds(20));
+		Thread.sleep(3000);
+		bm.logInfo(driver, "login button clicked");
+		String expectedURL = "https://www.alfadock-pack.com/ver10/#/home";
+		String actualURL = driver.getCurrentUrl();
+		Thread.sleep(3000);
+		// Create WebDriverWait instance with a timeout of 10 seconds
+		bm.wait(driver, Duration.ofSeconds(20));
+
+		// Wait until the element with id "myButton" is clickable
+		bm.waitForElementToBeClickable(By.xpath("//img[@src='assets/icons/Digital 5S.png']"));
+		Assert.assertEquals(expectedURL, actualURL);
+
+		if (expectedURL.equals(actualURL)) {
+			System.out.println("Login successful!");
+			bm.logPASS(driver, "Login successful!");
+		} else {
+			System.out.println("Login failed!");
+			bm.logFAIL(driver, "Login failed!");
+		}
+
 	}
 
 	@AfterClass
-	public void tearDown() throws InterruptedException {
+	public void tearDown() throws InterruptedException, EmailException {
 		bm.endTest(driver);
 		bm.endreport(driver);
 		bm.browserclose(driver);
+		System.out.println("Browser closed and report generated");
+
+		 MailWithAttachment mwa = new MailWithAttachment();
+		 mwa.sendReport();
 	}
 
 }
